8dd95d882abb3395907b2cbe0337c2f8dbed983e
火轮 <liuzhe@cai-inc.com>
2021-12-07 21:13:28 +0800

    数组

--- helloWorld.c
+++ helloWorld.c
@@ -3,7 +3,9 @@
 main() //程序入口
 {
 //    f();
-    funcFor();
+//    funcFor();
+//    funcChar();
+funcArray();
 }
 
 f()
@@ -33,3 +35,39 @@ funcFor()
     for (fahr = 0; fahr <= 300; fahr = fahr + 20)   // for 循环后面没用大括号
         printf("%3d %6.1f\n",fahr,(5.0/9.0)*(fahr - 32));
 }
+
+funcChar()
+{
+    int c; // int类型可以保存char类型数据
+    putchar(EOF); // 打印字符 EOF 相当于mac上的control + d
+    putchar('AA'); // 只会打印一个A
+    printf("%d",'AA'); // 单引号表示字符常量， 值为对应的编码值
+    while ((c = getchar()) != EOF) { //getchar接收来自键盘的输入
+        putchar(c);
+    }
+}
+
+funcArray()
+{
+    int c, i, nwhite, nother;
+    int ndigit[10]; //数组声明  长度为10
+
+    nwhite = nother = 0;
+    for (i = 0; i<10; ++i)
+        ndigit[i] = 0;
+    while ((c=getchar()) != EOF) {
+        if (c >= '0' && c<= '9') {
+            ++ndigit[c-'0'];
+        } else if (c == ' '||c== '\n' || c == '\t') {
+            ++nwhite;
+        } else {
+            ++nother;
+        }
+    }
+    printf("digits =");
+    for (i=0;i<10;i++) {
+        printf(" %d",ndigit[i]);
+        printf(",white space = %d, other = %d\n",nwhite,nother);
+    }
+}
+
